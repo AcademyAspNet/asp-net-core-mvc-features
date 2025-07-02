04db6ebc1f5aa9e792b606e3935015fed5dab050
AcademyAspNet <elprofesor.espana@gmail.com>
2025-07-02 10:45:37 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
